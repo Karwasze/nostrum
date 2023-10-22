aec60d27208485ff637d647d59d3ab3a8dd918a7
Karol Kędzierski <karolkedzierski@live.com>
2023-10-22 21:04:21 +0200

    Update ratelimiter.ex

--- lib/nostrum/api/ratelimiter.ex
+++ lib/nostrum/api/ratelimiter.ex
@@ -40,7 +40,7 @@ defmodule Nostrum.Api.Ratelimiter do
     open_opts = %{retry: 1_000_000_000, tls_opts: Constants.gun_tls_opts()}
     {:ok, conn_pid} = :gun.open(domain, 443, open_opts)
 
-    {:ok, :http2} = :gun.await_up(conn_pid)
+    {:ok, _} = :gun.await_up(conn_pid)
 
     # Start the old route cleanup loop
     Process.send_after(self(), :remove_old_buckets, :timer.hours(1))
